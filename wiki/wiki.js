c24f1f44f59b12192cbb5874f50d55280786ca36
corentindubois58-wq <corentindubois58@gmail.com>
2026-02-11 10:34:29 +0100

    Create wiki.js

---
+++ wiki/wiki.js
@@ -0,0 +1,12 @@
+function searchWiki() {
+  let input = document.getElementById("searchInput").value.toLowerCase();
+  let links = document.querySelectorAll(".menu a");
+
+  links.forEach(link => {
+    if (link.textContent.toLowerCase().includes(input)) {
+      link.style.display = "block";
+    } else {
+      link.style.display = "none";
+    }
+  });
+}
