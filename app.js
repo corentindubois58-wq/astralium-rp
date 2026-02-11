2f2d018ff7d3311ad67279efc8eaeefc9df7c159
corentindubois58-wq <corentindubois58@gmail.com>
2026-02-11 12:03:55 +0100

    z

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-/* FOOTER STICKY */
-let lastScroll = 0;
-const footer = document.getElementById("sticky-footer");
-
-window.addEventListener("scroll", ()=>{
-  const current = window.scrollY;
-  if(current > lastScroll && current > 300){
-    footer.classList.add("show");
-  }else{
-    footer.classList.remove("show");
-  }
-  lastScroll = current;
-});
-
-/* REGLEMENT */
 function checkRules(e){
   if(localStorage.getItem("rulesAccepted") !== "true"){
     e.preventDefault();
@@ -21,21 +6,3 @@ function checkRules(e){
     window.open("https://cfx.re/join/6avj7j","_blank");
   }
 }
-
-/* AVIS */
-const form = document.getElementById("reviewForm");
-const reviews = document.getElementById("reviews");
-
-form.addEventListener("submit", e=>{
-  e.preventDefault();
-
-  const pseudo = document.getElementById("pseudo").value;
-  const msg = document.getElementById("message").value;
-
-  const div = document.createElement("div");
-  div.className="review";
-  div.innerHTML = `<strong>${pseudo}</strong><p>${msg}</p>`;
-
-  reviews.prepend(div);
-  form.reset();
-});
